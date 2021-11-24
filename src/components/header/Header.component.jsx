94ed8db8101efcaaa94156038cf230a30ac75501
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-11-23 21:47:36 -0700

    add pending and done sections to header nav section

--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -7,8 +7,9 @@ export default function Header(props) {
       <h1 className="ui header center aligned">React ToDo App</h1>
       <nav class="ui secondary pointing menu">
         <a class="item active">All</a>
-        <a class="item">Completed</a>
         <a class="item">Top 3</a>
+        <a class="item">Pending</a>
+        <a class="item">Done</a>
       </nav>
     </div>
   );
