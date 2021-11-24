86889f83b70a68ccb4d0c4bd08dd6771632a594e
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-11-23 19:22:42 -0700

    add inital skeleton to header component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 
 // Import Components
 import Header from "./components/header/Header.component";
+import AddTodo from "./components/add-todo/AddTodo.component";
+import TodoList from "./components/todo-list/TodoList.component";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ui raised very padded text container segment">
       <Header />
+      <AddTodo />
+      <TodoList />
     </div>
   );
 }
--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -3,9 +3,13 @@ import "./Header.styles.css";
 
 export default function Header(props) {
   return (
-    <>
-      <h1 className="ui header">Header Works!</h1>
-      <button class="ui button">Follow</button>
-    </>
+    <div>
+      <h1 className="ui header center aligned">React ToDo App</h1>
+      <div class="ui secondary pointing menu">
+        <a class="item active">All</a>
+        <a class="item">Completed</a>
+        <a class="item">Top 3</a>
+      </div>
+    </div>
   );
 }
