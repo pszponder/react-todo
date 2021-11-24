88a0ca7d3d41d617ef3e793957b8a71fb41a4820
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-11-23 19:34:36 -0700

    add placeholder TodoItem values into TodoList component

--- src/components/todo-list/TodoList.component.jsx
+++ src/components/todo-list/TodoList.component.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "./TodoList.styles.css";
+import TodoItem from "./todo-item/TodoItem.component";
 
 export default function TodoList(props) {
-  return <h1>TodoList Works!</h1>;
+  return (
+    <div>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </div>
+  );
 }
