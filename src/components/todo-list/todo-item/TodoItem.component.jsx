a5b158b899473330ad45cba7f7dca5703a0623fa
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-11-24 12:11:35 -0700

    update class to className

--- src/components/todo-list/todo-item/TodoItem.component.jsx
+++ src/components/todo-list/todo-item/TodoItem.component.jsx
@@ -3,20 +3,20 @@ import "./TodoItem.styles.css";
 
 export default function TodoItem(props) {
   return (
-    <div class="todoItem ui segment teal tertiary">
-      <h3 class="ui header">Todo Description</h3>
-      <div class="ui small basic icon buttons">
-        <button class="ui button">
-          <i class="exclamation icon"></i>
+    <div className="todoItem ui segment teal tertiary">
+      <h3 className="ui header">Todo Description</h3>
+      <div className="ui small basic icon buttons">
+        <button className="ui button">
+          <i className="exclamation icon"></i>
         </button>
-        <button class="ui button">
-          <i class="edit icon"></i>
+        <button className="ui button">
+          <i className="edit icon"></i>
         </button>
-        <button class="ui button">
-          <i class="check icon"></i>
+        <button className="ui button">
+          <i className="check icon"></i>
         </button>
-        <button class="ui button">
-          <i class="delete icon"></i>
+        <button className="ui button">
+          <i className="delete icon"></i>
         </button>
       </div>
     </div>
