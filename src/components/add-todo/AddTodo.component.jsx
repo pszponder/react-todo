d5c1960073137f8047088b2434ffc18da832c226
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-11-23 19:32:17 -0700

    add initial html and css skeleton to AddTodo component

--- src/components/add-todo/AddTodo.component.jsx
+++ src/components/add-todo/AddTodo.component.jsx
@@ -2,5 +2,10 @@ import React from "react";
 import "./AddTodo.styles.css";
 
 export default function AddTodo(props) {
-  return <h1>AddTodo Works!</h1>;
+  return (
+    <div className="addTodo ui fluid action input">
+      <input type="text" placeholder="Add ToDo..." />
+      <button className="ui button">Add</button>
+    </div>
+  );
 }
